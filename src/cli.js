9492650a0b45bc80f416d55733d893050abfc517
PenLogos <142206877+PenLogos@users.noreply.github.com>
2023-11-26 19:47:46 -0500

    mejoras en cli.js y creación de node-cli.js

--- src/cli.js
+++ src/cli.js
@@ -1,11 +1,13 @@
 #!/usr/bin/env node
+
 const { mdLinks } = require('./index');
 
-const stats = process.argv.includes('--stats')
-const validate = process.argv.includes('--validate')
 const link = process.argv[2]
-console.log(validate, link);
+const validate = process.argv.includes('--validate')
+const stats = process.argv.includes('--stats')
+console.log(link, validate, stats);
 
 mdLinks(link, validate, stats)
 .then(res => console.log(res))
 .catch(error => console.log(error))
+
---
+++ src/node-cli.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+const { mdLinks } = require('./index');
+
+mdLinks('Archivos-de-prueba/Plantilla-README.md', true, true)
+.then(res => console.log(res, 'Archivo con links'))
+.catch(error => console.log(error, 'Archivo con links'))
+
+mdLinks('Archivos-de-prueba/Prueba-simple.text', false, true)
+.then(res => console.log(res, 'Archivo sin links'))
+.catch(error => console.log(error, 'Archivo sin links'))
